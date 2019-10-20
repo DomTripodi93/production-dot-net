0f4dadd55ef87b758e67e5ebcbfd65513e0e5b1b
DomTripodi93 <dtripodi93@gmail.com>
2019-10-19 20:43:31 -0400

    updated auth interceptor, and added documentation

--- frontend/src/app/shared/auth-interceptor.service.ts
+++ frontend/src/app/shared/auth-interceptor.service.ts
@@ -7,7 +7,7 @@ export class AuthInterceptorService implements HttpInterceptor{
     constructor(private auth: AuthService){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler){
-        if (req.url != this.auth.apiUrl +"auth/register/" && req.url != this.auth.apiUrl +"auth/login/"){
+        if (req.url != this.auth.authApiUrl +"auth/register/" && req.url != this.auth.authApiUrl +"auth/login/"){
             const authenticatedRequest = req.clone({
                 headers: req.headers.append("Authorization", 'Bearer ' + this.auth.token)
             })
@@ -15,5 +15,7 @@ export class AuthInterceptorService implements HttpInterceptor{
         } else {
             return next.handle(req)
         }
+        //Checks outgoing HTTP requests, and appends Authorization headers if they are not registration or login
+        // requests 
     }
 }
