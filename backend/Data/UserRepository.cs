30df50e9c6fcae3cfd0ca7aefda842d64df9f47d
DomTripodi93 <dtripodi93@gmail.com>
2020-01-25 17:33:45 -0500

    updated changelog return value ordering

--- backend/Data/UserRepository.cs
+++ backend/Data/UserRepository.cs
@@ -52,7 +52,8 @@ namespace BackEnd.Data
         {
             var changes = _context.ChangeLogs
                 .Where(m => m.userId == userId)
-                .Where(c => c.ChangedModel == model);
+                .Where(c => c.ChangedModel == model)
+                .OrderByDescending(c => c.TimeStamp);
 
             return await PagedList<ChangeLog>.CreateAsync(changes, changeLogParams.PageNumber, changeLogParams.PageSize);
         }
